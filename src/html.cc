6115efc0927e3027340d4f1f0617953fd9b78bdf
Elsa Sarrias <elsa.sar.ali@gmail.com>
2020-11-02 08:49:22 +0100

    make processHTML tidier

--- src/html.cc
+++ src/html.cc
@@ -4,20 +4,39 @@ namespace warc2text {
 
     std::unordered_set<std::string> startNL ( {"ul", "ol", "dl", "tr"} );
     std::unordered_set<std::string> endNL ( {"p", "div", "li", "dd", "th", "td", "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9"} );
-    std::unordered_set<std::string> selfNL ( {"br"} );
     std::unordered_set<std::string> noText ( {"script", "noscript", "style", ""} );
+    std::unordered_set<std::string> voidTags ( {"area", "base", "br", "col", "command", "embed", "hr", "img", "input", "keygen", "link", "meta", "param", "source", "track", "wbr"} );
 
+    inline bool isStartTag(const char* tag) { return startNL.find(tag) != startNL.end(); }
+    inline bool isEndTag(const char* tag) { return endNL.find(tag) != endNL.end(); }
+    inline bool isNoText(const char* tag) { return noText.find(tag) != noText.end(); }
+    inline bool isVoidTag(const char* tag) { return voidTags.find(tag) != voidTags.end(); }
+
+    // true if doc is ok
+    bool filter(markup::scanner& sc, const util::umap_tag_filters& tagFilters) {
+        util::umap_tag_filters::const_iterator tag_it = tagFilters.find(sc.get_tag_name());
+        if (tag_it == tagFilters.cend())
+            return true;
+        util::umap_attr_filters::const_iterator attr_it = tag_it->second.find(sc.get_attr_name());
+        if (attr_it == tag_it->second.cend())
+            return true;
+        for (const std::string& filter : attr_it->second){
+            if (strstr(sc.get_value(), filter.c_str())) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     int processHTML(const std::string& html, std::string& plaintext, const util::umap_tag_filters& tagFilters){
         plaintext = "";
         markup::instream si(html.c_str());
         markup::scanner sc(si);
 
-        util::umap_tag_filters::const_iterator tag_it;
-        util::umap_attr_filters::const_iterator attr_it;
-        int t = markup::scanner::TT_SPACE;
+        int t = markup::scanner::TT_SPACE; // just start somewhere that isn't ERROR or EOF
         int retval = util::SUCCESS;
-        while (t != markup::scanner::TT_EOF && t != markup::scanner::TT_ERROR) {
+
+        while (t != markup::scanner::TT_EOF and t != markup::scanner::TT_ERROR) {
             t = sc.get_token();
             switch (t) {
                 case markup::scanner::TT_ERROR:
@@ -25,40 +44,25 @@ namespace warc2text {
                 case markup::scanner::TT_EOF:
                     break;
                 case markup::scanner::TT_TAG_START:
-                    if (startNL.find(sc.get_tag_name()) != startNL.end()) {
+                    if (isStartTag(sc.get_tag_name()))
                         plaintext.push_back('\n');
-                    }
                     break;
                 case markup::scanner::TT_TAG_END:
-                    if (endNL.find(sc.get_tag_name()) != endNL.end() or selfNL.find(sc.get_tag_name()) != selfNL.end()) {
+                    if (isEndTag(sc.get_tag_name()) or isVoidTag(sc.get_tag_name()))
                         plaintext.push_back('\n');
-                    } else {
+                    else
                         plaintext.push_back(' ');
-                    }
                     break;
                 case markup::scanner::TT_WORD:
-                    if (noText.find(sc.get_tag_name()) == noText.end()) {
-                        if (strcmp(sc.get_value(),"&nbsp;") != 0) {
+                    if (!isNoText(sc.get_tag_name()) and strcmp(sc.get_value(), "&nbsp;") != 0)
                             plaintext.append(sc.get_value());
-                        }
-                    }
                     break;
                 case markup::scanner::TT_SPACE:
                     plaintext.push_back(' ');
                     break;
                 case markup::scanner::TT_ATTR:
-                    tag_it = tagFilters.find(sc.get_tag_name());
-                    if (tag_it == tagFilters.cend())
-                        break;
-                    attr_it = tag_it->second.find(sc.get_attr_name());
-                    if (attr_it == tag_it->second.cend())
-                        break;
-                    for (const std::string& value : attr_it->second){
-                        if (strstr(sc.get_value(), value.c_str())) {
-                            retval = util::FILTERED_DOCUMENT_ERROR;
-                            break; // passing one filter is enough
-                        }
-                    }
+                    if (!filter(sc, tagFilters))
+                        retval = util::FILTERED_DOCUMENT_ERROR;
                     break;
                 default:
                     break;
@@ -66,7 +70,6 @@ namespace warc2text {
         }
         return retval;
     }
-
 
     void unescapeEntities(const std::string& plaintext, std::string& decoded) {
         char* decodedplaintext = new char [plaintext.size() + 1];
