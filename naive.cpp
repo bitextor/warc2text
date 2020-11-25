c18bb77050aaf5c2ed523798204d86596d6e4cc6
Leopoldo Pla <lpla@dlsi.ua.es>
2020-11-25 14:35:09 +0100

    Working with uncompressed Bitextor files. Block search is still wonky.

--- naive.cpp
+++ naive.cpp
@@ -1,8 +1,9 @@
-// The Naïve search algorithm in C++
+// The Naive search algorithm in C++
 
 #include <iostream>
 #include <vector>
 #include <map>
+#include <utility>
 #include <fstream>
 #include <sstream>
 #include <chrono>
@@ -29,33 +30,51 @@ int naivePatternSearch(std::string mainString, std::string pattern) {
 int main(int argc, char *argv[]) {
     auto start = std::chrono::high_resolution_clock::now();
     if (argc < 3) {
-        printf("Usage: Pass two file paths as command line arguments "
-               "to compare the edit distance between them.\n");return 1;
+        printf("Usage: Pass plain_text.gz, url.gz and deferred.gz from warc2text for two languages (so 6 files)"
+               "to match their content with the lines from a sentence aligner-tab separated file"
+               "given as last argument. This will generate a standoff annotation for those sentences or TU\n");return 1;
     }
 
     std::ifstream t_doc_text(argv[1]);
     std::ifstream t_doc_url(argv[2]);
+    std::ifstream t_doc_standoff(argv[3]);
     std::string doc_text;
     std::string doc_url;
-    std::unordered_map<std::string, std::vector<std::string>> m;
+    std::string doc_standoff;
+    std::unordered_map<std::string, std::vector<std::pair<std::string,std::string>>> source_blocks;
     while (std::getline(t_doc_url, doc_url))
     {
         std::getline(t_doc_text, doc_text);
+
         std::string decoded_doc_text;
         util::decodeBase64(doc_text,decoded_doc_text);
+
+        std::getline(t_doc_standoff, doc_standoff);
+
         std::istringstream ss(decoded_doc_text);
         std::string token;
+
+        std::istringstream sss(doc_standoff);
+        std::string standoff;
+
         while(std::getline(ss, token)) {
-            m[doc_url].push_back(token);
+            std::getline(sss, standoff, ';');
+            std::pair<std::string,std::string> PAIR1;
+            PAIR1.first = token;
+            PAIR1.second = standoff;
+            source_blocks[doc_url].push_back(PAIR1);
         }
     }
 
-    std::ifstream t_sentences(argv[3]);
+    std::ifstream t_sentences(argv[4]);
     std::string line_sent;
     std::vector<std::string> decoded_doc_lines;
-    int pos_index = 0;
-    int block_index = 0;
-    std::string prev_url;
+    int pos_index_sl = 0;
+    int block_index_sl = 0;
+    std::string prev_url_sl;
+    int pos_index_tl = 0;
+    int block_index_tl = 0;
+    std::string prev_url_tl;
     std::string line_parts[5];
 
     while (std::getline(t_sentences, line_sent))
@@ -65,21 +84,35 @@ int main(int argc, char *argv[]) {
         int i = 0;
         while(std::getline(ss, token, '\t')) {
             line_parts[i] = token;
+            i++;
         }
 
-        if (line_parts[0] != prev_url)
+        if (line_parts[0] != prev_url_sl)
         {
-            pos_index = 0;
-            block_index = 0;
+            pos_index_sl = 0;
+            block_index_sl = 0;
+        }
+        if (line_parts[1] != prev_url_tl)
+        {
+            pos_index_tl = 0;
+            block_index_tl = 0;
         }
 
-        pos_index = naivePatternSearch(m[line_parts[0]][block_index].substr(pos_index), line_sent);
+        pos_index_sl = naivePatternSearch(source_blocks[line_parts[0]][block_index_sl].first.substr(pos_index_sl), line_parts[2]);
+        pos_index_tl = naivePatternSearch(source_blocks[line_parts[1]][block_index_tl].first.substr(pos_index_tl), line_parts[3]);
 
-        prev_url = line_parts[0];
-        if (pos_index != -1)
-            std::cout << "Sentence '" << line_sent << "' found at block " << block_index << " at position: " << pos_index << std::endl;
+        prev_url_sl = line_parts[0];
+        prev_url_tl = line_parts[1];
+
+        if (pos_index_sl != -1)
+            std::cout << "SL Sentence '" << line_parts[2] << "' found at block " << block_index_sl << " at position: " << pos_index_sl << std::endl;
         else
-            std::cout << "Sentence could not be found (maybe a concatenated string from sentence aligner)" << std::endl;
+            std::cout << "SL Sentence could not be found (maybe a concatenated string from sentence aligner)" << std::endl;
+
+        if (pos_index_tl != -1)
+            std::cout << "TL Sentence '" << line_parts[3] << "' found at block " << block_index_tl << " at position: " << pos_index_tl << std::endl;
+        else
+            std::cout << "TL Sentence could not be found (maybe a concatenated string from sentence aligner)" << std::endl;
     }
 
     auto stop = std::chrono::high_resolution_clock::now();
